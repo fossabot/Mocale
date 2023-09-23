a90d5d7036fc0a2b4646e4ce9d7bdbf75f36229d
Alex <33064621+Axemasta@users.noreply.github.com>
2023-09-23 16:35:17 +0100

    Work In Progress Xaml Binding
    Got half of the binding property extension working. It doesn't update when translations do but its quite a cool idea

--- src/Mocale/Markup/LocalizeExtension.cs
+++ src/Mocale/Markup/LocalizeExtension.cs
@@ -1,4 +1,4 @@
-using System.ComponentModel;
+using System.Globalization;
 using Ardalis.GuardClauses;
 using Mocale.Managers;
 
@@ -10,6 +10,8 @@ public class LocalizeExtension : IMarkupExtension<BindingBase>
     private readonly ITranslatorManager translatorManager;
 
     public string? Key { get; set; }
+
+    public BindingBase? Binding { get; set; }
 
     public IValueConverter? Converter { get; set; }
 
@@ -27,17 +29,7 @@ public class LocalizeExtension : IMarkupExtension<BindingBase>
 
     public BindingBase ProvideValue(IServiceProvider serviceProvider)
     {
-        if (string.IsNullOrEmpty(Parameter))
-        {
-            return new Binding
-            {
-                Mode = BindingMode.OneWay,
-                Path = $"[{Key}]",
-                Source = translatorManager,
-                Converter = Converter,
-            };
-        }
-        else
+        if (!string.IsNullOrEmpty(Parameter))
         {
             var parameterTranslatorManager = new ParameterTranslatorManager(translatorManager);
 
@@ -51,10 +43,74 @@ public class LocalizeExtension : IMarkupExtension<BindingBase>
                 Converter = Converter,
             };
         }
+        else if (Binding is Binding originalBinding)
+        {
+            //var parameterTranslatorManager = new ParameterTranslatorManager(translatorManager);
+
+            //parameterTranslatorManager.SetParameters(new object[] { MyBinding });
+
+            var parameter = translatorManager.Translate(Key);
+
+            var converter = new MyBindingValueConverter();
+
+            return new Binding()
+            {
+                Source = originalBinding.Source,
+                Path = originalBinding.Path,
+                Converter = converter,
+                ConverterParameter = parameter,
+                FallbackValue = originalBinding.FallbackValue,
+                Mode = originalBinding.Mode,
+                StringFormat = originalBinding.StringFormat,
+                TargetNullValue = originalBinding.TargetNullValue,
+            };
+
+            //return MyBinding;
+            //var multiBinding = new MultiBinding();
+            //multiBinding.Bindings.Add(MyBinding);
+            //multiBinding.Bindings.Add(new Binding
+            //{
+            //    Mode = BindingMode.OneWay,
+            //    Path = $"[{Key}]",
+            //    Source = parameterTranslatorManager,
+            //    Converter = Converter,
+            //});
+
+            //return multiBinding;
+        }
+        else
+        {
+            return new Binding
+            {
+                Mode = BindingMode.OneWay,
+                Path = $"[{Key}]",
+                Source = translatorManager,
+                Converter = Converter,
+            };
+        }
     }
 
     object IMarkupExtension.ProvideValue(IServiceProvider serviceProvider)
     {
         return ProvideValue(serviceProvider);
+    }
+}
+
+public class MyBindingValueConverter : IValueConverter
+{
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        if (parameter is string translation)
+        {
+            return string.Format(translation, value);
+
+        }
+
+        throw new NotSupportedException();
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        throw new NotImplementedException();
     }
 }
